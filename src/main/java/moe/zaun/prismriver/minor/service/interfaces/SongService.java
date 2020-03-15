bf57c3eb03e445f7ba1ed42f31faa2e2e3d2cc5b
Alex Fence <alexfence.code@gmail.com>
2020-03-15 23:56:29 +0100

    add SongServcie

---
+++ src/main/java/moe/zaun/prismriver/minor/service/interfaces/SongService.java
@@ -0,0 +1,13 @@
+package moe.zaun.prismriver.minor.service.interfaces;
+
+import java.util.List;
+import moe.zaun.prismriver.minor.model.Song;
+
+public interface SongService {
+    public void addSong(Song song);
+    public Song getSongById(String id);
+    public int getSongCount();
+    public List<Song> getSongs();
+    public List<Song> getSongs(int start);
+    public List<Song> getSongs(int start, int limit);
+}
